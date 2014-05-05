7ece009dabb26548817c9e9f18e753346209dd8c
MrLoh <flojoto@hotmail.com>
2014-05-05 20:17:16 +0200

    finished U2

--- U2/A5.py
+++ U2/A5.py
@@ -13,21 +13,24 @@ def rand_dupl(mi,ma):
 year = 365
 
 def double_birthday():
+	'''Gibt aus nach wie vielen Versuchen zufällig zwei gleiche Geburtstage zugeordnet werden.'''
 	return rand_dupl(1,year)
 
 def repeat_double_birthday(iterations):
+	'''Gibt eine Liste L aus, in der für jedes Element L_i die Anzahl steht, wie häufig nach 0<=i<=356 Versuchen zufällig derselbe Geburtstag erzeugt wurde. Dabei wird '''
 	L = [0]*(year+1)
 	for i in range(iterations):
-		L[double_birthday()] = i
+		L[double_birthday()] += 1
 	return L
 
-def birthday_paradox(guests,iterations):
+def birthday_paradox(guests,iterations=10000):
+	'''Gibt die Wahrscheinlichkeit aus, mit der mindestens 2 aus einer gegebenen Anzahl von Menschen, am selben Tag Geburtstag haben. Optional kann die Anzahl der Iterationen für die Berechnung angegeben werden.'''
 	L = repeat_double_birthday(iterations)
-	# res = 0
-	# for i in range(guests):
-	# 	res += L[i]
-	# 	print('res:',res)
-	return L[guests]/iterations
+	res = 0
+	for i in range(guests):
+		res += L[i]
+	return res/iterations
 
-print(repeat_double_birthday(10000))
-# print(birthday_paradox(20,10000))
+# Test
+for i in [5,10,20,50,100]:
+	print("p(doppelter Geburtstag auf Party mit %d Gästen) = %.2f%%" % (i, birthday_paradox(i)*100))
